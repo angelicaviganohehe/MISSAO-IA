d9f06a7928bea3d27c598b299a4ac1a57b4b4268
angelicaviganohehe <106531341+angelicaviganohehe@users.noreply.github.com>
2024-07-26 11:45:29 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -25,11 +25,12 @@ const perguntas = [
         afirmação: "Afirmação da alternativa 3"
         },
        ]
+    },
     {
     enunciado: "Quais são os elementos que compõe a dança?",
-    alternativas: [],
+    alternativas: [,
     {
-        texto: " contrapeso",
+        texto: "contrapeso",
         afirmação: "Afirmação da alternativa 1"
         },
 
@@ -41,12 +42,30 @@ const perguntas = [
         afirmação: "Afirmação da alternativa 3"
          },
         ]
+    },
     
 
     {
     enunciado: "O teatro é uma linguagem de que arte?",
-    alternativas: ["Artes literárias,", "Artes cênicas ","Artes visuais","Artes músicais"],
-    },
+    alternativas: [,
+    {
+        texto: "Artes Literàrias",
+        afirmação: "Afirmação da alternativa 1"
+        },
+
+        {texto: "Artes Cênicas",
+        afirmação: "Afirmação da alternativa 2"
+         },
+
+        {texto: "Artes Visuais",
+        afirmação: "Afirmação da alternativa 3"
+         },
+
+         {texto: "Artes musicais",
+         afirmação: "Afirmação da alternativa 4"
+          },
+          ]
+   },
     ];
 
     let atual = 0;
@@ -62,6 +81,12 @@ const perguntas = [
             for (const alternativa of perguntaAtual.alternativas) {
                 const botaoAlternativas =document.createElement("button");
                 botaoAlternativas.textContent = alternativa;
+                botaoAlternativas.addEventListener("click", () =>respostaSelecionada(alternativa));
                 caixaAlternativas.appendChild(botaoAlternativas);
     }
-            
+    function respostaSelecionada(opcaoSelecionada) {
+        const afirmacoes = opcaoSelecionada.afirmação;
+        historiaFinal = afirmacoes;
+        atual++;
+        mostraPergunta();
+        }     
